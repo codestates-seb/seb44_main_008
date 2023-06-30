5e2d62b2713f245138908e8a31c0eb78e781272a
otaeho <dhxogh123123@naver.com>
2023-06-30 17:43:36 +0900

    server initial setting

---
+++ server/moviepop/src/main/java/com/codestates/MoviepopApplication.java
@@ -0,0 +1,13 @@
+package com.codestates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviepopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviepopApplication.class, args);
+	}
+
+}
