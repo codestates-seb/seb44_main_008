4523128c93f8f72c32793cfb863617f18aaee8aa
plla2 <jhbljs92@naver.com>
2023-07-01 18:35:44 +0900

    ✨ Create Input Component

--- client/src/pages/Start.tsx
+++ client/src/pages/Start.tsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import { useState } from 'react';
 import Footer from '../components/Footer/Footer';
+import Input from '../components/Input/Input';
 
 const Start = () => {
+  const [email, setEmail] = useState<string>('');
+  const placeholder = '이메일';
+  const isValid = true;
   return (
     <div>
       Start
       <Footer />
+      <Input
+        value={email}
+        onChange={e => setEmail(e.target.value)}
+        placeholder={placeholder}
+        isvalid={isValid}
+      />
     </div>
   );
 };
