d4ced9c7255db6407f9bd92505ba83cfda7425e4
suemeeeee <yungmi2909@gmail.com>
2023-07-02 03:12:27 +0900

    📝 Button 공통 컴포넌트 完

---
+++ client/src/components/Button/Button.tsx
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+import { ButtonType } from "./type";
+
+const ButtonWrapper = styled.div`
+	width: fit-content;
+`
+const ButtonStyle = styled.button<ButtonType>`
+	width: 100%;
+	border: none;
+	border-radius: 0.5rem;
+	background-color: ${(props) => (props.type === 'variant' ? '#f20000' : '#232323')};
+	color: #FFFFFF;
+	padding: 0.7rem 1.3rem;
+
+	width: ${(props) => (props.width ? props.width : 'fit-content')};
+
+	&:hover {
+		background-color: ${(props) => (props.type === 'variant' ? '#c20000' : '#f20000')};
+    }
+`
+
+const Button = ( { value, type, width } : ButtonType ) => {
+	return (
+		<ButtonWrapper>
+		<ButtonStyle width={width} type={type}>{value}</ButtonStyle>
+		</ButtonWrapper>
+	)
+}
+
+export default Button;
