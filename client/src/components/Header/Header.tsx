0eef836a2cd2b784b7c6048d3ae3b4117f882a9b
gsjang94@gamil.com <gsjang94@gmail.com>
2023-07-03 10:45:14 +0900

    ⚡️ header 로그인 여부 버튼 생성

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -2,9 +2,9 @@ import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { RootState } from '../../redux/store/store';
 import styled from 'styled-components';
-import { GrSearch } from 'react-icons/gr';
 import LoginBtns from './LoginBtns';
 import UserArea from './UserArea';
+import SearchInput from './SearchInput';
 
 const HeaderWrapper = styled.header`
   width: 100%;
@@ -30,33 +30,6 @@ const Logo = styled.h1`
     letter-spacing: 1px;
   }
 `;
-const SearchInputBox = styled.div`
-  width: 40rem;
-  height: 3.4rem;
-  position: relative;
-  input {
-    width: 100%;
-    height: 100%;
-    background-color: var(--main-gray-color);
-    border: 0;
-    color: #565656;
-    border-radius: 10px;
-    padding: 0 0.87rem;
-    &:focus-visible {
-      outline: 0;
-    }
-  }
-  button {
-    position: absolute;
-    right: 0.87rem;
-    top: 50%;
-    transform: translateY(-50%);
-    font-size: 1.5rem;
-    path {
-      stroke: #9f9f9f;
-    }
-  }
-`;
 
 const Header = () => {
   const user = useSelector((state: RootState) => state.user);
@@ -66,16 +39,15 @@ const Header = () => {
         <Logo>
           <Link to="/main">MoviePOP</Link>
         </Logo>
-        <SearchInputBox>
-          <input
-            type="text"
-            placeholder="지금, 관심있는 영화를 검색해 보세요."
-          />
-          <button>
-            <GrSearch />
-          </button>
-        </SearchInputBox>
-        {user.isLoggedIn ? <UserArea /> : <LoginBtns />}
+
+        {user.isLoggedIn ? (
+          <>
+            <SearchInput />
+            <UserArea />
+          </>
+        ) : (
+          <LoginBtns />
+        )}
       </div>
     </HeaderWrapper>
   );
