8e30ebde6ea7c803833b18059dcfce6040485614
otaeho <dhxogh123123@naver.com>
2023-07-03 15:27:54 +0900

    feat: implement Comment entity(not implement mapping yet)

--- server/moviepop/src/main/java/com/codestates/comment/entity/Comment.java
+++ server/moviepop/src/main/java/com/codestates/comment/entity/Comment.java
@@ -1,2 +1,22 @@
-package com.codestates.comment.entity;public class Comment {
+package com.codestates.comment.entity;
+
+import com.codestates.audit.Auditable;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+public class Comment extends Auditable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long commentId;
+    @Column(nullable = false, length = 2000)
+    private String content;
+    @Column(nullable = false)
+    private Integer likes = 0;
 }
