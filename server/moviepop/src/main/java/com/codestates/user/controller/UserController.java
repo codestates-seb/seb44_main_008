e1368ecc167b5784667c9be6d234905bb80aa614
song4529 <gusdn4529@gmail.com>
2023-07-03 15:44:34 +0900

    feat : 유저에 사용되는 기능을 매개하는 Controller 구현

---
+++ server/moviepop/src/main/java/com/codestates/user/controller/UserController.java
@@ -0,0 +1,118 @@
+package com.codestates.user.controller;
+
+import com.codestates.dto.ResponseDto;
+import com.codestates.user.dto.UserDto;
+import com.codestates.user.entity.User;
+import com.codestates.user.mapper.UserMapper;
+import com.codestates.user.service.UserService;
+import com.codestates.utils.UriComponent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import javax.validation.constraints.Positive;
+import java.net.URI;
+
+@RestController
+@RequestMapping("/users")
+@Validated
+@Slf4j
+@RequiredArgsConstructor
+public class UserController {
+
+    private final static String USER_DEFAULT_URI = "/users";
+    private final UserService userService;
+    private final UserMapper userMapper;
+//    private final JwtTokenizer jwtTokenizer;
+
+    @PostMapping // 회원가입
+    public ResponseEntity postUser(@Valid @RequestBody UserDto.Post userPostDto) {
+        User user = userMapper.userPostDtoToUser(userPostDto);
+        User createUser = userService.createUser(user);
+
+        URI uri = UriComponent.createUri(USER_DEFAULT_URI, createUser.getUserId());
+
+        return ResponseEntity.created(uri).build();
+    }
+
+//    @GetMapping // 자신의 회원정보 조회
+//    public ResponseEntity getUser() {
+//        //로그인된 사용자 정보를 가져와야 가능해보임
+//        return new ResponseEntity(
+//                new ResponseDto.SingleResponseDto<>(userMapper.userToUserResponseDto(토큰정보)),
+//                HttpStatus.OK
+//        );
+//    }
+
+    @GetMapping("/{user-id}") // 특정 회원정보 조회
+    public ResponseEntity getOtherUser(@PathVariable("user-id") @Positive long userId) {
+        User user = userService.findUser(userId);
+
+        return new ResponseEntity<>(
+                new ResponseDto.SingleResponseDto<>(userMapper.userToUserResponseDto(user)),
+                HttpStatus.OK
+        );
+    }
+
+//    @GetMapping("/brief") // 간략한 회원 정보 조회
+//    public ResponseEntity getUserBrief() {
+//        //로그인된 사용자 정보를 가져와야 가능해보임
+//        return new ResponseEntity<>(
+//                new ResponseDto.SingleResponseDto<>(userMapper.userToUserBriefResponseDto(토큰정보)),
+//                HttpStatus.OK
+//        );
+//    }
+
+    @PatchMapping("/{user-id}") // 회원정보 수정 -> jwt적용하지 않았을 경우 userId가 필요해보임
+    public ResponseEntity patchUser(@PathVariable ("user-id}") @Positive long UserId,
+                                    @Valid @RequestBody UserDto.Patch userPatchDto) {
+        User user = userService.updateUser(userMapper.userPatchDtoToUser(userPatchDto));
+
+        return new ResponseEntity<>(
+                new ResponseDto.SingleResponseDto<>(userMapper.userToUserBriefResponseDto(user)),
+                HttpStatus.OK
+        );
+    }
+
+    @PatchMapping("/password") // 비밀번호 수정
+    public ResponseEntity patchUserPassword(@Positive long userId,
+                                            @Valid @RequestBody UserDto.PatchPassword userPatchPasswordDto) {
+        User user = userService.updateUserPassword(userId, userPatchPasswordDto.getCurrentPassword(), userPatchPasswordDto.getNewPassword());
+
+        return new ResponseEntity<>(
+                new ResponseDto.SingleResponseDto<>(userMapper.userToUserResponseDto(user)),
+                        HttpStatus.OK
+        );
+    }
+
+    @DeleteMapping // 회원 삭제
+    public ResponseEntity deleteUser(@Positive long userId) {
+        userService.deleteUser(userId);
+
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @PostMapping("/reviewBoards/{review-id}") // 게시글 찜 등록
+    public ResponseEntity postUserWish() {
+        return null;
+    }
+
+    @PostMapping("/groups/{group-id}") // 팟 참여 기능
+    public ResponseEntity postUserParticipation() {
+        return null;
+    }
+
+    @DeleteMapping("/reviewBoards/{review-id}") // 게시글 찜 해제
+    public ResponseEntity deleteReviewWish() {
+        return null;
+    }
+
+    @DeleteMapping("/groups/{group-id}") // 팟 참여 취소
+    public ResponseEntity deleteParticipatedGroup() {
+        return null;
+    }
+}
