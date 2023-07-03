8cbee9616cbb9516d3cd9d80a2ae87258e0c845e
otaeho <dhxogh123123@naver.com>
2023-07-03 15:27:15 +0900

    feat: implement dto for comment(post, patch, response...)

--- server/moviepop/src/main/java/com/codestates/comment/dto/CommentDto.java
+++ server/moviepop/src/main/java/com/codestates/comment/dto/CommentDto.java
@@ -1,2 +1,43 @@
-package com.codestates.comment.dto;public class CommentDto {
+package com.codestates.comment.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+
+public class CommentDto {
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class Post {
+        @NotBlank
+        private String content;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public static class Patch {
+        @Setter
+        long commentId;
+        @NotBlank
+        private String content;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public static class PatchResponse {
+        private long commentId;
+        private String content;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public static class Response {
+        private long commentId;
+        private String content;
+        private int likes;
+        private String createdAt;
+    }
 }
