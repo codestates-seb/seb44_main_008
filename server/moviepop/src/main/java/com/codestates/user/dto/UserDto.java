cf0eba67793953bbce3551e30c977b22a9961ebc
song4529 <gusdn4529@gmail.com>
2023-07-03 15:43:17 +0900

    feat : Post, Patch, PatchPassword, Response에 필요한 Dto 구현

---
+++ server/moviepop/src/main/java/com/codestates/user/dto/UserDto.java
@@ -0,0 +1,63 @@
+package com.codestates.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+public class UserDto {
+    @Getter
+    @AllArgsConstructor
+    public static class Post{
+        @NotBlank
+        @Email
+        private String email;
+
+        @NotBlank
+        private String nickname;
+
+        @NotBlank
+        private String profileImage;
+
+        @NotBlank
+        private String birth;
+
+        @NotBlank
+        private String password;
+
+//        @NotBlank
+//        private Tag tag;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public static class Patch{
+        @NotBlank
+        private String nickname;
+
+//        @NotBlank
+//        private Tag tag;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public static class PatchPassword{
+        @NotBlank
+        private String currentPassword;
+
+        @NotBlank
+        private String newPassword;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public static class Response{
+        private Long userId;
+        private String name;
+        private String nickname;
+        private String email;
+        private String profileImage;
+//        private Tag tag;
+    }
+}
