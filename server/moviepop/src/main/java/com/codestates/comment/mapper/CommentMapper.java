713e23e1727328852760c60ccef5fe7b176bd846
otaeho <dhxogh123123@naver.com>
2023-07-03 15:28:34 +0900

    feat: implement mapper for comment(post, patch <-> entity, entity <-> responseDto)

--- server/moviepop/src/main/java/com/codestates/comment/mapper/CommentMapper.java
+++ server/moviepop/src/main/java/com/codestates/comment/mapper/CommentMapper.java
@@ -1,2 +1,16 @@
-package com.codestates.comment.mapper;public class CommentMapper {
+package com.codestates.comment.mapper;
+
+import com.codestates.comment.dto.CommentDto;
+import com.codestates.comment.entity.Comment;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface CommentMapper {
+    Comment commentPostDtoToComment(CommentDto.Post postDto);
+    Comment commentPatchDtoToComment(CommentDto.Patch patchDto);
+    CommentDto.PatchResponse commentToCommentPatchResponseDto(Comment comment);
+    CommentDto.Response commentToCommentResponseDto(Comment comment);
+    List<CommentDto.Response> commentsToCommentResponseDtos(List<Comment> comments);
 }
