c8502e6feb06102d4682022f802956b44f1f8e55
otaeho <dhxogh123123@naver.com>
2023-07-03 15:21:27 +0900

    feat: add dto used in response

--- server/moviepop/src/main/java/com/codestates/dto/ResponseDto.java
+++ server/moviepop/src/main/java/com/codestates/dto/ResponseDto.java
@@ -1,2 +1,26 @@
-package com.codestates.dto;public class ResponseDto {
+package com.codestates.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+public class ResponseDto {
+    @Getter
+    @AllArgsConstructor
+    public static class SingleResponseDto<T> {
+        private T data;
+    }
+
+    @Getter
+    public static class MultipleResponseDto<T> {
+        private List<T> data;
+        private PageInfo pageInfo;
+
+        public MultipleResponseDto(List<T> data, Page page) {
+            this.data = data;
+            this.pageInfo = new PageInfo(page.getNumber(), page.getSize(), page.getTotalElements(), page.getTotalPages());
+        }
+    }
 }
