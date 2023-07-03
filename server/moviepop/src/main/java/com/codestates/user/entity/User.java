39031439d886455eb7ecc4f9fefe87977abe6674
song4529 <gusdn4529@gmail.com>
2023-07-03 15:45:35 +0900

    feat : User에 필요한 필요한 구성요소를 포함한 Entity 구현

---
+++ server/moviepop/src/main/java/com/codestates/user/entity/User.java
@@ -0,0 +1,75 @@
+package com.codestates.user.entity;
+
+import com.codestates.audit.Auditable;
+import com.codestates.exception.BusinessLogicException;
+import com.codestates.exception.ExceptionCode;
+import com.codestates.utils.CustomBeanUtils;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+public class User extends Auditable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long userId;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String nickname;
+
+    @Column(nullable = false, length = 100)
+    private String password;
+
+    @Column(nullable = false)
+    private String profileImage;
+
+    private String birth;
+
+    private float star = 0;
+
+    private String created_at;
+
+    private String last_modified_at;
+
+    @Enumerated(value = EnumType.STRING)
+    @Column(nullable = false)
+    private UserStatus userStatus = UserStatus.USER_ACTIVE;
+
+    public enum UserStatus {
+        USER_ACTIVE ("활동중인 계정"),
+        USER_SLEEP("휴면 계정"),
+        USER_WITHDRAW("탈퇴한 계정");
+
+        @Getter
+        private String status;
+
+        UserStatus(String status) {
+            this.status = status;
+        }
+    }
+
+//    @OneToMany
+//    @JoinColumn(name = "tag_id")
+//    private Tag tag;
+//
+//    @OneToMany
+//    @JoinColumn(name = "group_id")
+//    private Group group;
+
+    public User changeUserInfo(User sourceUser, CustomBeanUtils<User> beanUtils) {
+        return beanUtils.copyNonNullProperties(sourceUser, this);
+    }
+
+    public static void checkExistEmail (User targetUser) {
+        if(targetUser != null) throw new BusinessLogicException(ExceptionCode.USER_EXISTS);
+    }
+}
