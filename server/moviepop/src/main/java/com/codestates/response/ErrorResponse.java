4e5f2f2b17d28192a7848d14ed0e9292d01bfdb4
otaeho <dhxogh123123@naver.com>
2023-07-03 15:23:16 +0900

    feat: add custom class for error response(error info)

--- server/moviepop/src/main/java/com/codestates/response/ErrorResponse.java
+++ server/moviepop/src/main/java/com/codestates/response/ErrorResponse.java
@@ -1,2 +1,99 @@
-package com.codestates.response;public class ErrorResponse {
+package com.codestates.response;
+
+import com.codestates.exception.ExceptionCode;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+
+import javax.validation.ConstraintViolation;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Getter
+public class ErrorResponse {
+    private int status;
+    private String message;
+    private List<FieldError> fieldErrors;
+    private List<ConstraintViolationError> violationErrors;
+
+    private ErrorResponse(int status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+
+    private ErrorResponse(List<FieldError> fieldErrors, List<ConstraintViolationError> violationErrors) {
+        this.fieldErrors = fieldErrors;
+        this.violationErrors = violationErrors;
+    }
+
+    public static ErrorResponse of(HttpStatus status) {
+        return new ErrorResponse(status.value(), status.getReasonPhrase());
+    }
+
+    public static ErrorResponse of(HttpStatus status, String message) {
+        return new ErrorResponse(status.value(), message);
+    }
+
+    public static ErrorResponse of(BindingResult bindingResult) {
+        return new ErrorResponse(FieldError.of(bindingResult), null);
+    }
+
+    public static ErrorResponse of(Set<ConstraintViolation<?>> violations) {
+        return new ErrorResponse(null, ConstraintViolationError.of(violations));
+    }
+
+    public static ErrorResponse of(ExceptionCode exceptionCode) {
+        return new ErrorResponse(exceptionCode.getStatus(), exceptionCode.getMessage());
+    }
+
+    @Getter
+    public static class FieldError {
+        private String field;
+        private Object rejectedValue;
+        private String reason;
+
+        private FieldError(String field, Object rejectedValue, String reason) {
+            this.field = field;
+            this.rejectedValue = rejectedValue;
+            this.reason = reason;
+        }
+
+        public static List<FieldError> of(BindingResult bindingResult) {
+            final List<org.springframework.validation.FieldError> fieldErrors =
+                    bindingResult.getFieldErrors();
+
+            return fieldErrors.stream()
+                    .map(error -> new FieldError(
+                            error.getField(),
+                            error.getRejectedValue(),
+                            error.getDefaultMessage()
+                    ))
+                    .collect(Collectors.toList());
+        }
+    }
+
+    @Getter
+    public static class ConstraintViolationError {
+        private String propertyPath;
+        private Object rejectedValue;
+        private String reason;
+
+        private ConstraintViolationError(String propertyPath, Object rejectedValue, String reason) {
+            this.propertyPath = propertyPath;
+            this.rejectedValue = rejectedValue;
+            this.reason = reason;
+        }
+
+        public static List<ConstraintViolationError> of(Set<ConstraintViolation<?>> constraintViolations) {
+            return constraintViolations.stream()
+                    .map(error -> new ConstraintViolationError(
+                            error.getPropertyPath().toString(),
+                            error.getInvalidValue(),
+                            error.getMessage()
+                    ))
+                    .collect(Collectors.toList());
+        }
+    }
 }
