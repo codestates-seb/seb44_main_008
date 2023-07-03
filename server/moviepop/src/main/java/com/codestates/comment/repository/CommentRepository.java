c85a6736bb37412f9da1e4276da3e2002f2da59a
otaeho <dhxogh123123@naver.com>
2023-07-03 15:29:09 +0900

    feat: add repository for comment

--- server/moviepop/src/main/java/com/codestates/comment/repository/CommentRepository.java
+++ server/moviepop/src/main/java/com/codestates/comment/repository/CommentRepository.java
@@ -1,2 +1,7 @@
-package com.codestates.comment.repository;public class CommentMapper {
+package com.codestates.comment.repository;
+
+import com.codestates.comment.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
 }
