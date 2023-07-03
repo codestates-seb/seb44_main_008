735020ad7d1d4f66c20b43c297a0588f80ef7f17
otaeho <dhxogh123123@naver.com>
2023-07-03 15:19:12 +0900

    feat: add JPA Audit for Entity(created_at, last_modified_at)

--- server/moviepop/src/main/java/com/codestates/MoviepopApplication.java
+++ server/moviepop/src/main/java/com/codestates/MoviepopApplication.java
@@ -2,7 +2,9 @@ package com.codestates;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class MoviepopApplication {
 
