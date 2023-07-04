c8aa6a4eb56fe70f3be885dd1595de986d2990b3
otaeho <dhxogh123123@naver.com>
2023-07-04 18:26:49 +0900

    feat: add new mapping method for added dto

--- server/moviepop/src/main/java/com/codestates/reviewBoard/mapper/ReviewBoardMapper.java
+++ server/moviepop/src/main/java/com/codestates/reviewBoard/mapper/ReviewBoardMapper.java
@@ -1,10 +1,15 @@
 package com.codestates.reviewBoard.mapper;
 
+import com.codestates.comment.dto.CommentDto;
+import com.codestates.comment.mapper.CommentMapper;
 import com.codestates.reviewBoard.dto.ReviewBoardDto;
 import com.codestates.reviewBoard.entity.ReviewBoard;
+import com.codestates.reviewBoard.service.ReviewBoardService;
 import org.mapstruct.Mapper;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring")
 public interface ReviewBoardMapper {
@@ -12,4 +17,33 @@ public interface ReviewBoardMapper {
     ReviewBoard PatchToReviewBoard(ReviewBoardDto.Patch patch);
     ReviewBoardDto.Response reviewBoardToResponse(ReviewBoard reviewBoard);
     List<ReviewBoardDto.Response> reviewBoardsToResponses(List<ReviewBoard> reviewBoards);
+    ReviewBoardDto.EntireResponse reviewBoardToEntireResponse(ReviewBoard reviewBoard);
+    List<ReviewBoardDto.EntireResponse> reviewBoardsToEntireResponses(List<ReviewBoard> reviewBoards);
+    default ReviewBoardDto.DetailResponse reviewBoardToDetailResponse(ReviewBoard reviewBoard, CommentMapper commentMapper) {
+        List<CommentDto.Response> commentResponse = reviewBoard.getComments().stream()
+                .map(comment -> commentMapper.commentToCommentResponseDto(comment))
+                .collect(Collectors.toList());
+
+        ReviewBoardDto.DetailResponse detailResponse = ReviewBoardDto.DetailResponse.builder()
+                .reviewBoardId(reviewBoard.getReviewBoardId())
+                .title(reviewBoard.getTitle())
+                .review(reviewBoard.getReview())
+                .thumbnail(reviewBoard.getThumbnail())
+                .wish(reviewBoard.getWish())
+                .createdAt(reviewBoard.getCreatedAt())
+                .comments(commentResponse)
+                .build();
+
+        return detailResponse;
+    }
+
+    default ReviewBoardDto.MainResponse reviewBoardsToDetailResponses(List<ReviewBoard> reviewBoards, List<ReviewBoard> popularBoards) {
+        List<ReviewBoardDto.EntireResponse> reviewBoardEntire = reviewBoardsToEntireResponses(reviewBoards);
+        List<ReviewBoardDto.EntireResponse> popularReviewBoardEntire = reviewBoardsToEntireResponses(popularBoards);
+
+        return ReviewBoardDto.MainResponse.builder()
+                .popularBoards(popularReviewBoardEntire)
+                .boards(reviewBoardEntire)
+                .build();
+    }
 }
