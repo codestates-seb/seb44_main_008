99817aa9d935b27bff1cbeb4f8623717ac2e5f9e
song4529 <gusdn4529@gmail.com>
2023-07-04 13:41:31 +0900

    feat : ReviewBoardWishRepository 추가 및 찜 기능 유효성 검사를 위함 메서드 추가

---
+++ server/moviepop/src/main/java/com/codestates/user/repository/ReviewBoardWishRepository.java
@@ -0,0 +1,10 @@
+package com.codestates.user.repository;
+
+import com.codestates.reviewBoard.entity.ReviewBoard;
+import com.codestates.user.entity.ReviewBoardWish;
+import com.codestates.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReviewBoardWishRepository extends JpaRepository<ReviewBoardWish, Long> {
+    boolean existsByReviewBoardAndUser(ReviewBoard reviewBoard, User user);
+}
