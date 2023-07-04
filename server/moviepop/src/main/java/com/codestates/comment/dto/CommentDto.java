d847f5456ece9a45b4845f8a0a0f83594a0178c0
otaeho <dhxogh123123@naver.com>
2023-07-04 18:22:46 +0900

    fix: change format of createdAt

--- server/moviepop/src/main/java/com/codestates/comment/dto/CommentDto.java
+++ server/moviepop/src/main/java/com/codestates/comment/dto/CommentDto.java
@@ -1,11 +1,13 @@
 package com.codestates.comment.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
 
 public class CommentDto {
     @Getter
@@ -38,6 +40,7 @@ public class CommentDto {
         private long commentId;
         private String content;
         private int likes;
-        private String createdAt;
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm", timezone = "Asia/Seoul")
+        private LocalDateTime createdAt;
     }
 }
