b5244f321261ac6f8320ec8a4728e84b301e9751
otaeho <dhxogh123123@naver.com>
2023-07-04 13:15:53 +0900

    fix: remove imported class not used

--- server/moviepop/src/main/java/com/codestates/audit/Auditable.java
+++ server/moviepop/src/main/java/com/codestates/audit/Auditable.java
@@ -2,7 +2,6 @@ package com.codestates.audit;
 
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
