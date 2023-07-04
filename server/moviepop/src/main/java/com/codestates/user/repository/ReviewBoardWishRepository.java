8ffec620c5a1a60d50625ccece144f06e4afae33
song4529 <gusdn4529@gmail.com>
2023-07-04 13:59:59 +0900

    fix : 불필요한 import 제거

--- server/moviepop/src/main/java/com/codestates/user/repository/ReviewBoardWishRepository.java
+++ server/moviepop/src/main/java/com/codestates/user/repository/ReviewBoardWishRepository.java
@@ -5,6 +5,9 @@ import com.codestates.user.entity.ReviewBoardWish;
 import com.codestates.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ReviewBoardWishRepository extends JpaRepository<ReviewBoardWish, Long> {
+    ReviewBoardWish findByReviewBoardIdAndUserId(long reviewBoardId, long userId);
     boolean existsByReviewBoardAndUser(ReviewBoard reviewBoard, User user);
 }
