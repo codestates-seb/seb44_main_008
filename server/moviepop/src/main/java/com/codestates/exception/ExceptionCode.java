c1c949189ae80f8040e9730ddf2b5f9087eb2a3b
song4529 <gusdn4529@gmail.com>
2023-07-04 13:37:41 +0900

    fix : 새로운 ExceptionCode 추가

--- server/moviepop/src/main/java/com/codestates/exception/ExceptionCode.java
+++ server/moviepop/src/main/java/com/codestates/exception/ExceptionCode.java
@@ -3,10 +3,12 @@ package com.codestates.exception;
 import lombok.Getter;
 
 public enum ExceptionCode {
-    USER_NOT_FOUND(404, "유저가 존재하지않습니다."),
+    USER_NOT_FOUND(404, "유저가 존재하지않습니다"),
     USER_EXISTS(409, "이미 유저가 존재합니다"),
     PASSWORD_INCORRECT(403, "비밀번호가 일치하지 않습니다"),
-    INVALID_USER(400, "존재하지않는 유저입니다");
+    INVALID_USER(400, "존재하지않는 유저입니다"),
+    ALREADY_WISH_EXIST(409, "이미 찜하기가 되어있습니다"),
+    WISH_NOT_FOUND(404, "찜하기가 되어있지 않습니다");
 
     @Getter
     private int status;
