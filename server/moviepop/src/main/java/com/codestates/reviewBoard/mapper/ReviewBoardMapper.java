16745155b0d1c3ca54ad03179c17c95f98fb5fd6
otaeho <dhxogh123123@naver.com>
2023-07-04 18:39:45 +0900

    fix: change reviewBoardToResponse since new field is added

--- server/moviepop/src/main/java/com/codestates/reviewBoard/mapper/ReviewBoardMapper.java
+++ server/moviepop/src/main/java/com/codestates/reviewBoard/mapper/ReviewBoardMapper.java
@@ -2,6 +2,7 @@ package com.codestates.reviewBoard.mapper;
 
 import com.codestates.reviewBoard.dto.ReviewBoardDto;
 import com.codestates.reviewBoard.entity.ReviewBoard;
+import com.codestates.user.dto.UserDto;
 import org.mapstruct.Mapper;
 
 import java.util.List;
@@ -10,7 +11,25 @@ import java.util.List;
 public interface ReviewBoardMapper {
     ReviewBoard PostToReviewBoard(ReviewBoardDto.Post post);
     ReviewBoard PatchToReviewBoard(ReviewBoardDto.Patch patch);
-    ReviewBoardDto.Response reviewBoardToResponse(ReviewBoard reviewBoard);
+    default ReviewBoardDto.Response reviewBoardToResponse(ReviewBoard reviewBoard) {
+        UserDto.ReviewBoardResponse userResponse = new UserDto.ReviewBoardResponse(
+                reviewBoard.getUser().getUserId(),
+                reviewBoard.getUser().getNickname(),
+                reviewBoard.getUser().getProfileImage()
+        );
+
+        ReviewBoardDto.Response response = ReviewBoardDto.Response.builder()
+                .reviewBoardId(reviewBoard.getReviewBoardId())
+                .title(reviewBoard.getTitle())
+                .review(reviewBoard.getReview())
+                .wish(reviewBoard.getWish())
+                .thumbnail(reviewBoard.getThumbnail())
+                .createdAt(reviewBoard.getCreatedAt())
+                .user(userResponse)
+                .build();
+
+        return response;
+    }
     List<ReviewBoardDto.Response> reviewBoardsToResponses(List<ReviewBoard> reviewBoards);
     ReviewBoardDto.WishResponse reviewBoardToWishResponse(ReviewBoard reviewBoard);
 }
