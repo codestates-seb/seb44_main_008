3758d5cb416db621596870a1da9b90f8f87cda93
otaeho <dhxogh123123@naver.com>
2023-07-05 17:52:17 +0900

    feat: make movie service that has two methods(insert all init data in DB and select all data in DB)

--- server/moviepop/src/main/java/com/codestates/movie/service/MovieService.java
+++ server/moviepop/src/main/java/com/codestates/movie/service/MovieService.java
@@ -1,2 +1,29 @@
-package com.codestates.movie.service;public class MovieService {
+package com.codestates.movie.service;
+
+import com.codestates.movie.entity.Movie;
+import com.codestates.movie.repository.MovieRepository;
+import com.codestates.tag.entity.Tag;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Service
+@Transactional
+public class MovieService {
+    private final MovieRepository movieRepository;
+
+    public MovieService(MovieRepository movieRepository) {
+        this.movieRepository = movieRepository;
+    }
+
+    public void makeInitData(List<Movie> movieList) {
+        movieRepository.saveAll(movieList);
+    }
+
+    public Set<Movie> findMovies() {
+        return movieRepository.findAll().stream().collect(Collectors.toSet());
+    }
 }
