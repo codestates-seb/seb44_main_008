6debebfeaa39b8dcbae7ab0be81c65b74b51a6a5
otaeho <dhxogh123123@naver.com>
2023-07-05 15:07:39 +0900

    feat: add new movie info class

---
+++ server/moviepop/src/main/java/com/codestates/movie/entity/Movie.java
@@ -0,0 +1,28 @@
+package com.codestates.movie.entity;
+
+import lombok.AllArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@AllArgsConstructor
+public class Movie {
+    private String movieCode;
+    private String title;
+    private String genre;
+    private String isAdulted;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Movie movie = (Movie) o;
+        return movieCode == movie.movieCode;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(movieCode);
+    }
+}
