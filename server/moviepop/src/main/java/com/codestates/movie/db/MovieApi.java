c6ca21c7553f76e410b0bb091b6faf4971898cb5
otaeho <dhxogh123123@naver.com>
2023-07-05 15:08:36 +0900

    feat: add function that receives movie detail info and add thread

--- server/moviepop/src/main/java/com/codestates/movie/db/MovieApi.java
+++ server/moviepop/src/main/java/com/codestates/movie/db/MovieApi.java
@@ -1,5 +1,6 @@
 package com.codestates.movie.db;
 
+import com.codestates.movie.entity.Movie;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -11,41 +12,51 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import java.text.SimpleDateFormat;
+import java.time.format.DateTimeFormatter;
 import java.util.*;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class MovieApi {
-    public static List<String> getMovieList() {
+    public static Set<String> getMovieList() {
         HashMap<String, Object> result = new HashMap<String, Object>();
-        String jsonInString = "";
-        List<String> movieCodes = new ArrayList<>();
+        Set<String> movieCodeSet = new HashSet<>();
+
+        ExecutorService executorService = Executors.newFixedThreadPool(5); // 동시 요청 수
 
         try {
-            for(int page = 1; page <= 5; page++) {
+            List<Future<ResponseEntity<Map>>> futures = new ArrayList<>();
+
+            SimpleDateFormat formatter = new SimpleDateFormat();
+            formatter.applyPattern("yyyyMMdd");
+            Calendar calendar = Calendar.getInstance();
+            calendar.set(2023, 7, 2);
+
+            for(int count = 1; count <= 100; count++) {
                 RestTemplate restTemplate = new RestTemplate();
 
                 HttpHeaders header = new HttpHeaders();
                 HttpEntity<?> entity = new HttpEntity<>(header);
-                String url = "http://kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieList.json";
-                UriComponents uri = UriComponentsBuilder.fromHttpUrl(url+"?"+"curPage="+page+"&itemPerPage=100&"+"key=f5eef3421c602c6cb7ea224104795888&targetDt=20230704").build();
+                String url = "http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchWeeklyBoxOfficeList.json";
+                UriComponents uri = UriComponentsBuilder.fromHttpUrl(url+"?"+"targetDt="+formatter.format(calendar.getTime())+"&weekGb=0&"+"key=f5eef3421c602c6cb7ea224104795888").build();
 
                 //이 한줄의 코드로 API를 호출해 MAP타입으로 전달 받는다.
-                ResponseEntity<Map> resultMap = restTemplate.exchange(uri.toString(), HttpMethod.GET, entity, Map.class);
-                result.put("statusCode", resultMap.getStatusCodeValue()); //http status code를 확인
-                result.put("header", resultMap.getHeaders()); //헤더 정보 확인
-                result.put("body", resultMap.getBody()); //실제 데이터 정보 확인
-
-                LinkedHashMap lm = (LinkedHashMap) resultMap.getBody().get("movieListResult");
-                ArrayList<Map> dboxoffList = (ArrayList<Map>) lm.get("movieList");
-                for(int idx = 0; idx < dboxoffList.size(); idx++)
-                    movieCodes.add(dboxoffList.get(idx).get("movieCd").toString());
-
-                //데이터를 제대로 전달 받았는지 확인 string형태로 파싱해줌
-                ObjectMapper mapper = new ObjectMapper();
-                jsonInString = mapper.writeValueAsString(resultMap.getBody());
-                System.out.println(jsonInString);
+                Callable<ResponseEntity<Map>> resultMap = () -> restTemplate.exchange(uri.toString(), HttpMethod.GET, entity, Map.class);
+                Future<ResponseEntity<Map>> future = executorService.submit(resultMap);
+                futures.add(future);
+                calendar.add(Calendar.MONTH, -1);
             }
 
-
+            for(Future<ResponseEntity<Map>> future : futures) {
+                ResponseEntity<Map> resultMap = future.get();
+                LinkedHashMap lm = (LinkedHashMap) resultMap.getBody().get("boxOfficeResult");
+                ArrayList<Map> dboxoffList = (ArrayList<Map>) lm.get("weeklyBoxOfficeList");
+                for(int idx = 0; idx < dboxoffList.size(); idx++)
+                    movieCodeSet.add(dboxoffList.get(idx).get("movieCd").toString());
+            }
         } catch (HttpClientErrorException | HttpServerErrorException e) {
             result.put("statusCode", e.getRawStatusCode());
             result.put("body"  , e.getStatusText());
@@ -55,14 +66,70 @@ public class MovieApi {
             result.put("statusCode", "999");
             result.put("body"  , "excpetion오류");
             System.out.println(e.toString());
+        } finally {
+            executorService.shutdown();
         }
-        return movieCodes;
+
+        return movieCodeSet;
     }
 
-//    public static void
+    public static List<Movie> getMovieDetail(Set<String> movieCodes) {
+        HashMap<String, Object> result = new HashMap<String, Object>();
+        List<Movie> movieList = new ArrayList<>();
+
+        ExecutorService executorService = Executors.newFixedThreadPool(10); // 동시 요청 수
+
+        try {
+            List<Future<ResponseEntity<Map>>> futures = new ArrayList<>();
+
+            SimpleDateFormat formatter = new SimpleDateFormat();
+            formatter.applyPattern("yyyyMMdd");
+            Calendar calendar = Calendar.getInstance();
+            calendar.set(2023, 7, 2);
+
+            for(String movieCode : movieCodes) {
+                RestTemplate restTemplate = new RestTemplate();
+
+                HttpHeaders header = new HttpHeaders();
+                HttpEntity<?> entity = new HttpEntity<>(header);
+                String url = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieInfo.json";
+                UriComponents uri = UriComponentsBuilder.fromHttpUrl(url+"?"+"movieCd="+movieCode+"&key=f5eef3421c602c6cb7ea224104795888").build();
+
+                Callable<ResponseEntity<Map>> resultMap = () -> restTemplate.exchange(uri.toString(), HttpMethod.GET, entity, Map.class);
+                Future<ResponseEntity<Map>> future = executorService.submit(resultMap);
+                futures.add(future);
+                calendar.add(Calendar.MONTH, -1);
+            }
+
+            for(Future<ResponseEntity<Map>> future : futures) {
+                ResponseEntity<Map> resultMap = future.get();
+                LinkedHashMap lm = (LinkedHashMap) resultMap.getBody().get("movieInfoResult");
+                Map dboxoffList = (Map) lm.get("movieInfo");
+
+                ArrayList<String> genres = (ArrayList<String>)dboxoffList.get("genres");
+                ArrayList<String> audits = (ArrayList<String>)dboxoffList.get("audits");
+                movieList.add(new Movie(dboxoffList.get("movieCd").toString(), dboxoffList.get("movieNm").toString(), dboxoffList.get("genres").toString(), dboxoffList.get("audits").toString()));
+            }
+        } catch (HttpClientErrorException | HttpServerErrorException e) {
+            result.put("statusCode", e.getRawStatusCode());
+            result.put("body"  , e.getStatusText());
+            System.out.println(e.toString());
+
+        } catch (Exception e) {
+            result.put("statusCode", "999");
+            result.put("body"  , "excpetion오류");
+            System.out.println(e.toString());
+        } finally {
+            executorService.shutdown();
+        }
+
+        return movieList;
+    }
 
     public static void main(String[] args) {
-        List<String> movieCodes = getMovieList();
+        Set<String> movieCodes = getMovieList();
         System.out.println("movieCodes.size() = " + movieCodes.size());
+        List<Movie> movies = getMovieDetail(movieCodes);
+        System.out.println("movies.size() = " + movies.size());
     }
 }
