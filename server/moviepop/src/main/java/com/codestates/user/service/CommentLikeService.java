0bd82db18ebd0a20261d93cca7405c831e1127e3
44kwon <kwh3217@gmail.com>
2023-07-06 09:18:40 +0900

    merge 연관관계

---
+++ server/moviepop/src/main/java/com/codestates/user/service/CommentLikeService.java
@@ -0,0 +1,26 @@
+package com.codestates.user.service;
+
+import com.codestates.comment.entity.Comment;
+import com.codestates.user.entity.CommentLike;
+import com.codestates.user.entity.User;
+import com.codestates.user.repository.CommentLikeRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class CommentLikeService {
+    private final CommentLikeRepository commentLikeRepository;
+
+    public CommentLikeService(CommentLikeRepository commentLikeRepository) {
+        this.commentLikeRepository = commentLikeRepository;
+    }
+
+    CommentLike findByCommentAndUser(Comment comment, User user) {
+        return commentLikeRepository.findByCommentAndUser(comment, user);
+    }
+    boolean existsByCommentAndUser(Comment comment, User user) {
+        return commentLikeRepository.existsByCommentAndUser(comment,user);
+    }
+
+}
