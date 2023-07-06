0b45f8f9bbf8a25f611e4724c7d44e47f8e207fe
otaeho <dhxogh123123@naver.com>
2023-07-06 18:15:08 +0900

    fix: remove AllArgsConstructor annotation for serialization

--- server/moviepop/src/main/java/com/codestates/tag/dto/TagDto.java
+++ server/moviepop/src/main/java/com/codestates/tag/dto/TagDto.java
@@ -2,20 +2,18 @@ package com.codestates.tag.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 public class TagDto {
 
     @Getter
-    @Setter
     public static class response {
         private long tagId;
         private String tagName;
     }
 
     @Getter
-    @Setter
-    @AllArgsConstructor
     public static class ReviewBoardRequest {
         private long tagId;
     }
