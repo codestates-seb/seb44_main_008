5cd2a9c5381028ce52cb6157fc17054db304f47a
otaeho <dhxogh123123@naver.com>
2023-07-06 16:37:31 +0900

    fix: add exception code for review board, movie

--- server/moviepop/src/main/java/com/codestates/exception/ExceptionCode.java
+++ server/moviepop/src/main/java/com/codestates/exception/ExceptionCode.java
@@ -10,10 +10,12 @@ public enum ExceptionCode {
     INVALID_USER(400, "존재하지않는 유저입니다"),
     REVIEW_BOARD_NOT_FOUND(404, "게시글이 존재하지 않습니다"),
     CANNOT_UPDATE_REVIEW_BOARD(403, "게시글 작성자가 아닙니다"),
+    CANNOT_SHOW_REVIEW_BOARD(400, "미성년자는 열람할 수 없는 게시글입니다"),
     ALREADY_WISH_EXIST(409, "이미 찜하기가 되어있습니다"),
     WISH_NOT_FOUND(404, "찜하기가 되어있지 않습니다"),
-    COMMENT_NOT_FOUND(404, "Comment Not Found"),
+    COMMENT_NOT_FOUND(404, "댓글이 존재하지 않습니다"),
     CANNOT_UPDATE_COMMENT(403, "댓글 작성자가 아닙니다"),
+    MOVIE_NOT_FOUND(404, "영화가 존재하지 않습니다"),
     ALREADY_LIKE_EXIST(409, "이미 좋아요가 되어있습니다"),
     LIKE_NOT_FOUND(404, "좋아요가 되어있지 않습니다");
 
