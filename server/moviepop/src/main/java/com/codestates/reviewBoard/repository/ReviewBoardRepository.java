23ab0261b7f2491062b6d2ff7a528755b90b7bf1
otaeho <dhxogh123123@naver.com>
2023-07-06 16:46:38 +0900

    feat: add new select queries for R-rated review board

--- server/moviepop/src/main/java/com/codestates/reviewBoard/repository/ReviewBoardRepository.java
+++ server/moviepop/src/main/java/com/codestates/reviewBoard/repository/ReviewBoardRepository.java
@@ -1,12 +1,22 @@
 package com.codestates.reviewBoard.repository;
 
 import com.codestates.reviewBoard.entity.ReviewBoard;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ReviewBoardRepository extends JpaRepository<ReviewBoard,Long> {
     List<ReviewBoard> findTop12ByOrderByReviewBoardIdDesc();
+    @Query(value = "select r from ReviewBoard r inner join r.movie m where m.isAdulted = :isAdulted order by r.reviewBoardId desc")
+    List<ReviewBoard> findTop12ByOrderByReviewBoardIdDescByIsAdulted(boolean isAdulted);
     List<ReviewBoard> findTop8ByOrderByWishDesc();
+    @Query(value = "select r from ReviewBoard r inner join r.movie m where m.isAdulted = :isAdulted order by r.wish desc")
+    List<ReviewBoard> findTop8ByOrderByWishDescByIsAdulted(boolean isAdulted);
+    @Query(value = "select r from ReviewBoard r inner join r.movie m where m.isAdulted = :isAdulted")
+    Page<ReviewBoard> findAllByIsAdulted(boolean isAdulted, Pageable pageable);
 }
