d155fecdb97ddea7551227c3eca6576d4fd17162
otaeho <dhxogh123123@naver.com>
2023-07-06 18:17:49 +0900

    feat: add mapping reviewBoardRequest to ReviewBoardTag

--- server/moviepop/src/main/java/com/codestates/tag/mapper/TagMapper.java
+++ server/moviepop/src/main/java/com/codestates/tag/mapper/TagMapper.java
@@ -2,9 +2,11 @@ package com.codestates.tag.mapper;
 
 import com.codestates.reviewBoard.dto.ReviewBoardDto;
 import com.codestates.reviewBoard.entity.ReviewBoard;
+import com.codestates.reviewBoard.entity.ReviewBoardTag;
 import com.codestates.tag.dto.TagDto;
 import com.codestates.tag.entity.Tag;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.List;
 
@@ -16,4 +18,10 @@ public interface TagMapper {
     Tag requestToReviewBoardTag(TagDto.ReviewBoardRequest reviewBoardRequest);
 
     List<Tag> requestsToReviewBoardTags(List<TagDto.ReviewBoardRequest> reviewBoardRequests);
+
+    // List<TagDto.ReviewBoardRequest> -> List<ReviewBoardTag>
+
+    @Mapping(source = "tagId", target = "tag.tagId")
+    ReviewBoardTag reviewBoardRequestToReviewBoardTag(TagDto.ReviewBoardRequest request);
+    List<ReviewBoardTag> reviewBoardsRequestToReviewBoardTags(List<TagDto.ReviewBoardRequest> requests);
 }
