b7619888a46c80e14a39d0060ef46510fc098f5c
otaeho <dhxogh123123@naver.com>
2023-07-06 13:45:57 +0900

    fix: change repository name group to movie_party

--- server/moviepop/src/main/java/com/codestates/group/repository/MoviePartyRepository.java
+++ server/moviepop/src/main/java/com/codestates/group/repository/MoviePartyRepository.java
@@ -3,5 +3,5 @@ package com.codestates.group.repository;
 import com.codestates.group.entity.MovieParty;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface GroupRepository extends JpaRepository<MovieParty, Long> {
+public interface MoviePartyRepository extends JpaRepository<MovieParty, Long> {
 }
