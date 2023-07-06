a8d4df3454b218f9412f2159aec699328cac2c36
otaeho <dhxogh123123@naver.com>
2023-07-06 16:40:39 +0900

    fix: resolve conflict

--- server/moviepop/src/main/java/com/codestates/tag/entity/Tag.java
+++ server/moviepop/src/main/java/com/codestates/tag/entity/Tag.java
@@ -1,15 +1,11 @@
 package com.codestates.tag.entity;
 
-<<<<<<< HEAD
-=======
 import com.codestates.reviewBoard.entity.ReviewBoardTag;
 import com.codestates.user.entity.UserTag;
->>>>>>> 15fbec5c8b5341e55b2723cc04b81dd42cf21ec0
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-<<<<<<< HEAD
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -20,19 +16,10 @@ import java.util.Objects;
 @Getter
 @Setter
 @Entity
-=======
-import javax.persistence.*;
-
-@Getter
-@Setter
-@Entity
-@NoArgsConstructor
->>>>>>> 15fbec5c8b5341e55b2723cc04b81dd42cf21ec0
 public class Tag {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long tagId;
-<<<<<<< HEAD
     private String tagName;
 
     public Tag(String tagName) {
@@ -51,14 +38,10 @@ public class Tag {
     public int hashCode() {
         return Objects.hash(tagName);
     }
-=======
-
-    private String tagName;
 
 //    @OneToMany(mappedBy = "tag", cascade = CascadeType.ALL)
 //    private ReviewBoardTag reviewBoardTag;
 //
 //    @OneToMany(mappedBy = "tag", cascade = CascadeType.ALL)
 //    private UserTag userTag;
->>>>>>> 15fbec5c8b5341e55b2723cc04b81dd42cf21ec0
 }
