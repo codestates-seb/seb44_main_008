513f735ec56ed9b6272152ae80cd93a80c507754
otaeho <dhxogh123123@naver.com>
2023-07-06 13:35:46 +0900

    fix: change class and method name group to movie_party

--- server/moviepop/src/main/java/com/codestates/group/mapper/MoviePartyMapper.java
+++ server/moviepop/src/main/java/com/codestates/group/mapper/MoviePartyMapper.java
@@ -1,15 +1,15 @@
 package com.codestates.group.mapper;
 
-import com.codestates.group.dto.GroupDto;
+import com.codestates.group.dto.MoviePartyDto;
 import com.codestates.group.entity.MovieParty;
 import org.mapstruct.Mapper;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
-public interface GroupMapper {
-    MovieParty groupPostDtoToGroup(GroupDto.Post postDto);
-    MovieParty groupPatchDtoToGroup(GroupDto.Patch patchDto);
-    GroupDto.Response groupToGroupResponseDto(MovieParty group);
-    List<GroupDto.Response> groupsToGroupResponseDtos(List<MovieParty> groupList);
+public interface MoviePartyMapper {
+    MovieParty moviePartyPostDtoToMovieParty(MoviePartyDto.Post postDto);
+    MovieParty moviePartyPatchDtoToMovieParty(MoviePartyDto.Patch patchDto);
+    MoviePartyDto.Response moviePartyToMoviePartyResponseDto(MovieParty group);
+    List<MoviePartyDto.Response> moviePartiesToMoviePartyResponseDtos(List<MovieParty> groupList);
 }
