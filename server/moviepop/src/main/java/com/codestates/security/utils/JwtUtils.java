e6bd5341d53c41d2dcee7809865a050ecf9dd8de
otaeho <dhxogh123123@naver.com>
2023-07-10 16:47:41 +0900

    feat: add JwtUtils that extracts claims from token

--- server/moviepop/src/main/java/com/codestates/security/utils/JwtUtils.java
+++ server/moviepop/src/main/java/com/codestates/security/utils/JwtUtils.java
@@ -1,2 +1,21 @@
-package com.codestates.security.utils;public class JwtUtils {
+package com.codestates.security.utils;
+
+import com.codestates.security.jwt.JwtTokenizer;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+
+@Component
+public class JwtUtils {
+    private final JwtTokenizer jwtTokenizer;
+
+    public JwtUtils(JwtTokenizer jwtTokenizer) {
+        this.jwtTokenizer = jwtTokenizer;
+    }
+
+    public Map<String, Object> getJwsClaimsFromRequest(HttpServletRequest request) {
+        String jws = request.getHeader("Authorization").replace("Bearer ", "");
+        return jwtTokenizer.extractAllClaims(jws);
+    }
 }
