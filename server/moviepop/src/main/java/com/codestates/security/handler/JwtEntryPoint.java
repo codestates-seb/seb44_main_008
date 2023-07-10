c0b7ef13b63a8c061cfa529149ba71eb4719f149
otaeho <dhxogh123123@naver.com>
2023-07-10 16:57:49 +0900

    feat: add entry point for handling authentication exception

--- server/moviepop/src/main/java/com/codestates/security/handler/JwtEntryPoint.java
+++ server/moviepop/src/main/java/com/codestates/security/handler/JwtEntryPoint.java
@@ -1,2 +1,21 @@
-package com.codestates.security.handler;public class JwtEntryPoint {
+package com.codestates.security.handler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+@Component
+public class JwtEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        log.error("Unauthorized error: {}", authException.getMessage());
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Error: Unauthorized");
+    }
 }
