a7307f7691902065f71d67461bb8ce5082db3efd
otaeho <dhxogh123123@naver.com>
2023-07-10 17:37:04 +0900

    feat: add CacheKey class that is used in redis as key

--- server/moviepop/src/main/java/com/codestates/security/redis/CacheKey.java
+++ server/moviepop/src/main/java/com/codestates/security/redis/CacheKey.java
@@ -1,2 +1,9 @@
-package com.codestates.security.redis;public class CacheKey {
+package com.codestates.security.redis;
+
+import lombok.Getter;
+
+@Getter
+public class CacheKey {
+    public static final String USER = "user";
+    public static final int DEFAULT_EXPIRE_SEC = 60;
 }
