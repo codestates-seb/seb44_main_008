f6d0118daa59ca27080961c51cd975032a1c6344
otaeho <dhxogh123123@naver.com>
2023-07-10 16:36:52 +0900

    feat: add Authority entity for manage user's role

--- server/moviepop/src/main/java/com/codestates/security/entity/Authority.java
+++ server/moviepop/src/main/java/com/codestates/security/entity/Authority.java
@@ -1,2 +1,29 @@
-package com.codestates.security.entity;public class Authority {
+package com.codestates.security.entity;
+
+import com.codestates.user.entity.User;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.*;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Builder
+@Entity
+public class Authority implements GrantedAuthority {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long authorityId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "USER_ID")
+    private User user;
+
+    private String role;
+
+    @Override
+    public String getAuthority() {
+        return role;
+    }
 }
