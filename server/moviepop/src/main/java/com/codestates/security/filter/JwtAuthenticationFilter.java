dc32506c303f534f31371a24642d67260a3f50e7
otaeho <dhxogh123123@naver.com>
2023-07-10 16:57:15 +0900

    feat: implement custom authentication filter

--- server/moviepop/src/main/java/com/codestates/security/filter/JwtAuthenticationFilter.java
+++ server/moviepop/src/main/java/com/codestates/security/filter/JwtAuthenticationFilter.java
@@ -1,2 +1,74 @@
-package com.codestates.security.filter;public class JwtAuthenticationFilter {
+package com.codestates.security.filter;
+
+import com.codestates.security.jwt.JwtTokenizer;
+import com.codestates.security.redis.repository.LogoutAccessTokenRedisRepository;
+import com.codestates.security.userdetails.CustomUserDetailService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final JwtTokenizer jwtTokenizer;
+    private final CustomUserDetailService customUserDetailService;
+    private final LogoutAccessTokenRedisRepository logoutAccessTokenRedisRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String accessToken = getToken(request);
+        if(accessToken != null) {
+            checkLogout(accessToken);
+            String username = jwtTokenizer.getEmail(accessToken);
+
+            if(username != null) {
+                UserDetails userDetails = customUserDetailService.loadUserByUsername(username);
+                equalsUsernameFromTokenAndUserDetails(userDetails.getUsername(), username);
+                validateAccessToken(accessToken, userDetails);
+                processSecurity(request, userDetails);
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String getToken(HttpServletRequest request) {
+        String headerAuth = request.getHeader("Authorization");
+        if(StringUtils.hasText(headerAuth) && headerAuth.startsWith("Bearer "))
+            return headerAuth.substring(7);
+
+        return null;
+    }
+
+    private void checkLogout(String accessToken) {
+        if(logoutAccessTokenRedisRepository.existsById(accessToken))
+            throw new IllegalArgumentException("이미 로그아웃된 회원입니다.");
+    }
+
+    private void equalsUsernameFromTokenAndUserDetails(String userDetailsUsername, String tokenUsername) {
+        if(!userDetailsUsername.equals(tokenUsername))
+            throw new IllegalArgumentException("username이 토큰과 맞지 않습니다.");
+    }
+
+    private void validateAccessToken(String accessToken, UserDetails userDetails) {
+        if(!jwtTokenizer.validateToken(accessToken, userDetails))
+            throw new IllegalArgumentException("토큰 검증 실패!");
+    }
+
+    private void processSecurity(HttpServletRequest request, UserDetails userDetails) {
+        UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+        authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+        SecurityContextHolder.getContext().setAuthentication(authToken);
+    }
 }
