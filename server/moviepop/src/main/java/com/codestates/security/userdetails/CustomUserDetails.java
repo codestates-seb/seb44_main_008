f7446eba3db916b38c92f53d1c2ce7c06e4fd6e2
otaeho <dhxogh123123@naver.com>
2023-07-10 16:59:30 +0900

    feat: implement custom UserDetails

--- server/moviepop/src/main/java/com/codestates/security/userdetails/CustomUserDetails.java
+++ server/moviepop/src/main/java/com/codestates/security/userdetails/CustomUserDetails.java
@@ -1,2 +1,71 @@
-package com.codestates.security.userdetails;public class CustomUserDetails {
+package com.codestates.security.userdetails;
+
+import com.codestates.user.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CustomUserDetails implements UserDetails {
+    private String username;
+    private String password;
+    @Builder.Default
+    private List<String> roles = new ArrayList<>();
+
+    public static UserDetails of(User user) {
+        return CustomUserDetails.builder()
+                .username(user.getEmail())
+                .password(user.getPassword())
+                .roles(user.getRoles())
+                .build();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.stream()
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
 }
