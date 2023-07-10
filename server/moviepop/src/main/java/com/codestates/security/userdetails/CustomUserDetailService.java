53bbbb840c14b4137374696721c9c4dc2c9f4821
otaeho <dhxogh123123@naver.com>
2023-07-10 17:00:08 +0900

    feat: implement custom UserDetailsService

--- server/moviepop/src/main/java/com/codestates/security/userdetails/CustomUserDetailService.java
+++ server/moviepop/src/main/java/com/codestates/security/userdetails/CustomUserDetailService.java
@@ -1,2 +1,26 @@
-package com.codestates.security.userdetails;public class CustomUserDetailsService {
+package com.codestates.security.userdetails;
+
+import com.codestates.security.redis.CacheKey;
+import com.codestates.user.entity.User;
+import com.codestates.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    @Cacheable(value = CacheKey.USER, key = "#username", unless = "#result == null")
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsernameWithAuthority(username).orElseThrow(() -> new NoSuchElementException("없는 회원입니다."));
+        return CustomUserDetails.of(user);
+    }
 }
