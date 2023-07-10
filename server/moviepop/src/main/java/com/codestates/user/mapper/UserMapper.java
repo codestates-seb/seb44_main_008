90bf98fe26fe67a0ff3303af51ba078a2e42f61a
otaeho <dhxogh123123@naver.com>
2023-07-10 09:31:07 +0900

    fix: fix logic about UserResponseDto mapping because of new fields

--- server/moviepop/src/main/java/com/codestates/user/mapper/UserMapper.java
+++ server/moviepop/src/main/java/com/codestates/user/mapper/UserMapper.java
@@ -1,10 +1,16 @@
 package com.codestates.user.mapper;
 
+import com.codestates.movie_party.dto.MoviePartyDto;
+import com.codestates.movie_party.entity.MovieParty;
+import com.codestates.movie_party.mapper.MoviePartyMapper;
 import com.codestates.user.dto.UserDto;
 import com.codestates.user.dto.UserResponseDto;
 import com.codestates.user.entity.User;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingConstants;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface UserMapper {
@@ -12,7 +18,19 @@ public interface UserMapper {
     User userPatchDtoToUser(UserDto.Patch userPatchDto);
     UserDto.Response userToUserBriefResponseDto(User user);
 
-    UserResponseDto userToUserResponseDto(User user);
+    default UserResponseDto userToUserResponseDto(User user, MoviePartyMapper moviePartyMapper) {
+        List<MoviePartyDto.MyPageResponse> myRecruitingGroup = moviePartyMapper.moviePartiesToMyPageResponses(user.getParties());
 
+        List<MovieParty> participatingGroup = user.getMoviePartyUsers().stream()
+                .map(moviePartyUser -> moviePartyUser.getMovieParty())
+                .collect(Collectors.toList());
+        List<MoviePartyDto.MyPageResponse> myParticipatingGroup = moviePartyMapper.moviePartiesToMyPageResponses(participatingGroup);
+
+        UserResponseDto response = new UserResponseDto(userToUserBriefResponseDto(user), myRecruitingGroup, myParticipatingGroup);
+
+        return response;
+    }
+    UserDto.MoviePartyResponse userToMoviePartyResponseDto(User user);
+    List<UserDto.MoviePartyResponse> usersToMoviePartyResponseDtos(List<User> users);
     //default UserResponseDto
 }
