2bdb0bf694cfd5ec5722159e9c34c879617f23a5
otaeho <dhxogh123123@naver.com>
2023-07-10 09:26:00 +0900

    fix: add new movie party dtos for groups that I write and that I participate

--- server/moviepop/src/main/java/com/codestates/user/dto/UserResponseDto.java
+++ server/moviepop/src/main/java/com/codestates/user/dto/UserResponseDto.java
@@ -1,12 +1,16 @@
 package com.codestates.user.dto;
 
+import com.codestates.movie_party.dto.MoviePartyDto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+
+import java.util.List;
 
 @Getter
 @AllArgsConstructor
 public class UserResponseDto {
     private UserDto.Response user;
 //    private List<리뷰보드Dto> reviews;
-//    private List<그룹Dto> groups;
+    private List<MoviePartyDto.MyPageResponse> myRecruitingGroup;
+    private List<MoviePartyDto.MyPageResponse> myParticipatingGroup;
 }
