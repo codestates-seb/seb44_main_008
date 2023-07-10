2b70fadeb4280e9af410903463f3a714535e81f8
otaeho <dhxogh123123@naver.com>
2023-07-10 16:44:08 +0900

    feat: add headerUtil enum that indicates token type

--- server/moviepop/src/main/java/com/codestates/security/utils/JwtHeaderUtilEnums.java
+++ server/moviepop/src/main/java/com/codestates/security/utils/JwtHeaderUtilEnums.java
@@ -1,2 +1,12 @@
-package com.codestates.security.utils;public class JwtHeaderUtilEnums {
+package com.codestates.security.utils;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum JwtHeaderUtilEnums {
+    GRANT_TYPE("JWT 타입 / Bearer ", "Bearer ");
+    private String description;
+    private String value;
 }
