f1a7a4e02b5a3f6705473e88dd88ee220a6fefbc
otaeho <dhxogh123123@naver.com>
2023-07-10 16:34:50 +0900

    feat: add Login VO for authentication

--- server/moviepop/src/main/java/com/codestates/security/vo/Login.java
+++ server/moviepop/src/main/java/com/codestates/security/vo/Login.java
@@ -1,2 +1,13 @@
-package com.codestates.security.vo;public class Login {
+package com.codestates.security.vo;
+
+import lombok.Getter;
+
+import javax.validation.constraints.NotBlank;
+
+@Getter
+public class Login {
+    @NotBlank
+    private String email;
+    @NotBlank
+    private String password;
 }
