f956a4724bc72a29ebd33cfa6b5f02dc633da1ad
otaeho <dhxogh123123@naver.com>
2023-07-11 16:58:51 +0900

    feat: add configuration about file storage

--- server/moviepop/src/main/java/com/codestates/image/config/StorageConfiguration.java
+++ server/moviepop/src/main/java/com/codestates/image/config/StorageConfiguration.java
@@ -1,2 +1,25 @@
-package com.codestates.image.config;public class StorageConfiguration {
+package com.codestates.image.config;
+
+import com.codestates.image.service.S3StorageService;
+import com.codestates.image.service.StorageService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+@Configuration
+public class StorageConfiguration {
+    private static final String REGION = "ap-northeast-2";
+
+    @Bean
+    public StorageService s3StorageService() {
+        S3Client s3Client =
+                S3Client.builder()
+                        .region(Region.of(REGION))
+                        .credentialsProvider(DefaultCredentialsProvider.create())
+                        .build();
+
+        return new S3StorageService(s3Client);
+    }
 }
