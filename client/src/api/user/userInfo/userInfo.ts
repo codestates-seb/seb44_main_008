6c382a4c6950fc9a5cd4eaf874c2d1b72d135a7d
plla2 <jhbljs92@naver.com>
2023-07-12 14:08:24 +0900

    📝 react-query 설정

---
+++ client/src/api/user/userInfo/userInfo.ts
@@ -0,0 +1,67 @@
+import { instance } from '../../api';
+
+type UserType = {
+  userId: number;
+  name: string;
+  nickname: string;
+  email: string;
+  profileImage: string;
+  myTags: {
+    tagId: number;
+    tagName: string;
+  }[];
+  wishedBoard: {
+    reviewBoardId: number;
+    title: string;
+    movieTitle: string;
+    liked: boolean;
+    user: {
+      userId: number;
+      nickname: string;
+      profileImage: string;
+    };
+  }[];
+  myBoard: {
+    reviewBoardId: number;
+    title: string;
+    movieTitle: string;
+    user: {
+      userId: number;
+      nickname: string;
+      profileImage: string;
+    };
+  }[];
+  myRecruitingGroup: {
+    groupId: number;
+    title: string;
+    location: string;
+    maxCapacity: number;
+    currentParticipant: number;
+    meetingDate: string;
+    movieTitle: string;
+  }[];
+  myParticipatingGroup: {
+    groupId: number;
+    title: string;
+    location: string;
+    maxCapacity: number;
+    currentParticipant: number;
+    meetingDate: string;
+    movieTitle: string;
+  }[];
+};
+type PassType = {
+  currentPw: string;
+  newPw: string;
+};
+export const GetUser = (): Promise<UserType> =>
+  instance.get('/users').then(res => res.data);
+
+export const PatchUser = (data: any) => {
+  instance.patch(`/users`, data);
+};
+export const DeleteUser = () => instance.delete(`/users`);
+
+export const PatchPass = (data: PassType) => {
+  instance.patch(`/users/password`, data);
+};
