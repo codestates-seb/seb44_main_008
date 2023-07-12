3faf85ff1c078ce3b98988876a07d8a0902b8baf
otaeho <dhxogh123123@naver.com>
2023-07-12 11:12:55 +0900

    fix: add new Exception Code for verifying nickname

--- server/moviepop/src/main/java/com/codestates/exception/ExceptionCode.java
+++ server/moviepop/src/main/java/com/codestates/exception/ExceptionCode.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 public enum ExceptionCode {
     USER_NOT_FOUND(404, "유저가 존재하지 않습니다"),
     USER_EXISTS(409, "이미 유저가 존재합니다"),
+    USER_NICKNAME_EXISTS(409, "해당 닉네임은 중복되어 사용할 수 없습니다"),
     PASSWORD_INCORRECT(403, "비밀번호가 일치하지 않습니다"),
     INVALID_USER(400, "존재하지않는 유저입니다"),
     REVIEW_BOARD_NOT_FOUND(404, "게시글이 존재하지 않습니다"),
