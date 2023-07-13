f31e337a8ec454ef0a267ac042e31ac24eb75f63
otaeho <dhxogh123123@naver.com>
2023-07-13 17:47:46 +0900

    feat: add update and delete image logic and change return type of storing method

--- server/moviepop/src/main/java/com/codestates/image/service/S3StorageService.java
+++ server/moviepop/src/main/java/com/codestates/image/service/S3StorageService.java
@@ -1,9 +1,12 @@
 package com.codestates.image.service;
 
+import com.codestates.review_board.entity.ReviewBoard;
+import com.codestates.user.entity.User;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.multipart.MultipartFile;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import software.amazon.awssdk.services.s3.model.PutObjectResponse;
 
@@ -26,46 +29,38 @@ public class S3StorageService implements StorageService {
     }
 
     @Override
-    public void storeProfileImage(MultipartFile file) {
-        String key = makeS3OBjectKey(BUCKET_PROFILE_IMAGE_PATH, file);
-
-        PutObjectRequest request = createPutObjectRequest(BUCKET_NAME, key);
-
+    public String storeProfileImage(MultipartFile profileImage) {
         try {
-            final Path path = multipartFileToPath(file);
+            final Path path = multipartFileToPath(profileImage);
+            String key = makeS3OBjectKey(BUCKET_PROFILE_IMAGE_PATH, path);
+            PutObjectRequest request = createPutObjectRequest(BUCKET_NAME, key);
             PutObjectResponse response = s3Client.putObject(request, path);
 
             log.info("# File uploaded successfully. ETag: " + response.eTag());
+            return key;
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-//        finally {
-//            s3Client.close();
-//        }
     }
 
     @Override
-    public void storeThumbnailImage(MultipartFile file) {
-        String key = makeS3OBjectKey(BUCKET_THUMBNAIL_IMAGE_PATH, file);
-
-        PutObjectRequest request = createPutObjectRequest(BUCKET_NAME, key);
-
+    public String storeThumbnailImage(MultipartFile thumbnail) {
         try {
-            final Path path = multipartFileToPath(file);
+            final Path path = multipartFileToPath(thumbnail);
+            String key = makeS3OBjectKey(BUCKET_THUMBNAIL_IMAGE_PATH, path);
+            PutObjectRequest request = createPutObjectRequest(BUCKET_NAME, key);
             PutObjectResponse response = s3Client.putObject(request, path);
 
             log.info("# File uploaded successfully. ETag: " + response.eTag());
+            return key;
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-//        finally {
-//            s3Client.close();
-//        }
     }
 
-    private String makeS3OBjectKey(String bucketCoffeeImagePath, MultipartFile multipartFile) {
-        final String fileName = multipartFile.getOriginalFilename();
-        return bucketCoffeeImagePath.concat("/").concat(fileName);
+    private String makeS3OBjectKey(String bucketImagePath, Path path) {
+        final String fileName = path.getFileName().toString();
+        return bucketImagePath.concat("/").concat(fileName);
     }
 
     private PutObjectRequest createPutObjectRequest(String bucketName, String key) {
@@ -79,12 +74,47 @@ public class S3StorageService implements StorageService {
         Path path = null;
         try {
             path = Files.createTempFile("temp", file.getOriginalFilename());
-            System.out.println("path = " + path);
 
             Files.write(path, file.getBytes());
             return path;
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    @Override
+    public void deleteProfileImage(User user) {
+        s3Client.deleteObject(DeleteObjectRequest.builder()
+                .bucket(BUCKET_NAME)
+                .key(user.getProfileImage())
+                .build());
+    }
+
+    @Override
+    public void deleteThumbnailImage(ReviewBoard reviewBoard) {
+        s3Client.deleteObject(DeleteObjectRequest.builder()
+                .bucket(BUCKET_NAME)
+                .key(reviewBoard.getThumbnail())
+                .build());
+    }
+
+    @Override
+    public String updateProfileImage(MultipartFile profileImage, User user) {
+        if(user.getProfileImage() != null)
+            deleteProfileImage(user);
+
+        if(profileImage != null)
+            return storeProfileImage(profileImage);
+        return null;
+    }
+
+    @Override
+    public String updateThumbnailImage(MultipartFile thumbnail, ReviewBoard reviewBoard) {
+        if(reviewBoard.getThumbnail() != null)
+            deleteThumbnailImage(reviewBoard);
+
+        if(thumbnail != null)
+            return storeThumbnailImage(thumbnail);
+        return null;
     }
 }
