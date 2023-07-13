8c192858bbac90303f8571fab627bb9b76fc8d7f
otaeho <dhxogh123123@naver.com>
2023-07-13 11:58:06 +0900

    fix: remove UserTagRepository method

--- server/moviepop/src/main/java/com/codestates/user/repository/UserTagRepository.java
+++ server/moviepop/src/main/java/com/codestates/user/repository/UserTagRepository.java
@@ -5,5 +5,5 @@ import com.codestates.user.entity.UserTag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserTagRepository extends JpaRepository<UserTag, Long> {
-    Tag findByUserTag(UserTag userTag);
+//    Tag findByUserTag(UserTag userTag);
 }
