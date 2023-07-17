58e1768dd1660d620b22db087b3940d5c0fde6a0
plla2 <jhbljs92@naver.com>
2023-07-17 17:59:23 +0900

    🎨 경로 재설정, 새로고침 등등

--- client/src/api/user/userTab/userTab.ts
+++ client/src/api/user/userTab/userTab.ts
@@ -1,8 +1,9 @@
 import { instance } from '../../api';
 import { UserType } from '../userInfo/userInfo';
 
-export const GetTabA = (): Promise<UserType> =>
-  instance.get('/users').then(res => res.data.data);
+export const GetTabA = (): Promise<UserType> => {
+  return instance.get('/users').then(res => res.data.data);
+};
 
 export const DeleteTabA = (postId: number) =>
   instance.delete(`/users/reviewBoards/${postId}`).then(res => res.data);
