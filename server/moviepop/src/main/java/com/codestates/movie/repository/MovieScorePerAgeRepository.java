d87195cf881cdcae0b5b75976b1fd76194074393
otaeho <dhxogh123123@naver.com>
2023-07-19 12:02:17 +0900

    feat: add entity for counting review board count by movie and age

---
+++ server/moviepop/src/main/java/com/codestates/movie/repository/MovieScorePerAgeRepository.java
@@ -0,0 +1,11 @@
+package com.codestates.movie.repository;
+
+import com.codestates.movie.entity.Movie;
+import com.codestates.movie.entity.MovieScorePerAge;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MovieScorePerAgeRepository extends JpaRepository<MovieScorePerAge, Long> {
+    Optional<MovieScorePerAge> findByMovie(Movie movie);
+}
---
+++ server/moviepop/src/main/java/com/codestates/movie/service/MovieScorePerAgeService.java
@@ -0,0 +1,66 @@
+package com.codestates.movie.service;
+
+import com.codestates.exception.BusinessLogicException;
+import com.codestates.exception.ExceptionCode;
+import com.codestates.movie.entity.Movie;
+import com.codestates.movie.entity.MovieScorePerAge;
+import com.codestates.movie.repository.MovieScorePerAgeRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class MovieScorePerAgeService {
+    private final MovieScorePerAgeRepository movieScorePerAgeRepository;
+
+    public MovieScorePerAgeService(MovieScorePerAgeRepository movieScorePerAgeRepository) {
+        this.movieScorePerAgeRepository = movieScorePerAgeRepository;
+    }
+
+    public MovieScorePerAge addMovieScorePerAge(Movie movie, int ageRange) {
+        MovieScorePerAge movieScorePerAge = findMovieScorePerAgeByMovie(movie);
+
+        if(ageRange == 0) movieScorePerAge.setZero(movieScorePerAge.getZero() + 1);
+        else if(ageRange == 1) movieScorePerAge.setTen(movieScorePerAge.getTen() + 1);
+        else if(ageRange == 2) movieScorePerAge.setTwenty(movieScorePerAge.getTwenty() + 1);
+        else if(ageRange == 3) movieScorePerAge.setThirty(movieScorePerAge.getThirty() + 1);
+        else if(ageRange == 4) movieScorePerAge.setFourty(movieScorePerAge.getFourty() + 1);
+        else if(ageRange == 5) movieScorePerAge.setFifty(movieScorePerAge.getFifty() + 1);
+        else movieScorePerAge.setOther(movieScorePerAge.getOther() + 1);
+
+        return movieScorePerAgeRepository.save(movieScorePerAge);
+    }
+
+    public MovieScorePerAge subtractMovieScorePerAge(Movie movie, int ageRange) {
+        MovieScorePerAge movieScorePerAge = findMovieScorePerAgeByMovie(movie);
+
+        if(ageRange == 0) movieScorePerAge.setZero(movieScorePerAge.getZero() - 1);
+        else if(ageRange == 1) movieScorePerAge.setTen(movieScorePerAge.getTen() - 1);
+        else if(ageRange == 2) movieScorePerAge.setTwenty(movieScorePerAge.getTwenty() - 1);
+        else if(ageRange == 3) movieScorePerAge.setThirty(movieScorePerAge.getThirty() - 1);
+        else if(ageRange == 4) movieScorePerAge.setFourty(movieScorePerAge.getFourty() - 1);
+        else if(ageRange == 5) movieScorePerAge.setFifty(movieScorePerAge.getFifty() - 1);
+        else movieScorePerAge.setOther(movieScorePerAge.getOther() - 1);
+
+        return movieScorePerAgeRepository.save(movieScorePerAge);
+    }
+
+    public MovieScorePerAge findMovieScorePerAgeByMovie(Movie movie) {
+        return movieScorePerAgeRepository.findByMovie(movie).orElseThrow(() -> new BusinessLogicException(ExceptionCode.MOVIE_NOT_FOUND));
+    }
+
+    public int getMovieScorePerAge(Movie movie, int ageRange) {
+        MovieScorePerAge movieScorePerAge = findMovieScorePerAgeByMovie(movie);
+
+        int score = 0;
+        if(ageRange == 0) score = movieScorePerAge.getZero();
+        else if(ageRange == 1) score = movieScorePerAge.getTen();
+        else if(ageRange == 2) score = movieScorePerAge.getTwenty();
+        else if(ageRange == 3) score = movieScorePerAge.getThirty();
+        else if(ageRange == 4) score = movieScorePerAge.getFourty();
+        else if(ageRange == 5) score = movieScorePerAge.getFifty();
+        else score = movieScorePerAge.getOther();
+
+        return score;
+    }
+}
