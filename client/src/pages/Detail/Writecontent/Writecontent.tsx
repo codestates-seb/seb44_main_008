8fa535d7f5cdc2cdd4ec6d49ba212fb65738eb03
suemeeeee <yungmi2909@gmail.com>
2023-07-20 00:44:30 +0900

    🐛 리뷰글 작성 시 태그 선택 안하고 제출 시 유효성 걸리게 수정 & 비번 수정 디버깅

--- client/src/pages/Detail/Writecontent/Writecontent.tsx
+++ client/src/pages/Detail/Writecontent/Writecontent.tsx
@@ -104,37 +104,33 @@ const Writecontent = () => {
 
   const onClickSubmitButton = () => {
     // 유효성 검사
+    {
+      title.length === 0
+        ? setTitleErr(false)
+        : movieTitle.length === 0
+        ? setMovieTitleErr(false)
+        : selectedTags[0] === undefined
+        ? setTagErr(false)
+        : content.length <= 10
+        ? setContentErr(false)
+        : (() => {
+            const confirmed = window.confirm('게시글을 등록하시겠습니까?');
 
-    if (title.length === 0) {
-      setTitleErr(false);
-    }
-    if (movieTitle.length === 0) {
-      setMovieTitleErr(false);
-    }
-    if (selectedTags[0] === undefined) {
-      setTagErr(false);
-    }
-    if (content.length <= 10) {
-      setContentErr(false);
-    }
-    if (titleErr && movieTitle && TagErr && contentErr) {
-      const confirmed = window.confirm('게시글을 등록하시겠습니까?');
-      console.log(confirmed);
-
-      if (confirmed) {
-        const submitData = {
-          post: {
-            title: title,
-            movieId: Number(movieId),
-            review: content,
-            tags: selectedTags,
-          },
-          thumbnail: file ? file[0] : undefined,
-        };
-        writeMutations.mutate(submitData);
-        alert('게시글이 등록되었습니다.');
-        navigate('/main');
-      }
+            if (confirmed) {
+              const submitData = {
+                post: {
+                  title: title,
+                  movieId: Number(movieId),
+                  review: content,
+                  tags: selectedTags,
+                },
+                thumbnail: file ? file[0] : undefined,
+              };
+              writeMutations.mutate(submitData);
+              alert('게시글이 등록되었습니다.');
+              navigate('/main');
+            }
+          })();
     }
   };
 
