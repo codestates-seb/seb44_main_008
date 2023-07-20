8cf405978572eef43d0a61adb7bef308a9e9f8ff
otaeho <dhxogh123123@naver.com>
2023-07-20 17:31:29 +0900

    fix: add hashCode and equals for usage in HashSet

--- server/moviepop/src/main/java/com/codestates/review_board/entity/ReviewBoardRecentVisit.java
+++ server/moviepop/src/main/java/com/codestates/review_board/entity/ReviewBoardRecentVisit.java
@@ -7,6 +7,7 @@ import lombok.Setter;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @NoArgsConstructor
 @Getter
@@ -21,9 +22,22 @@ public class ReviewBoardRecentVisit {
     @JoinColumn(name = "user_id")
     private User user;
 
-    @OneToOne
+    @ManyToOne
     @JoinColumn(name = "review_board_id")
     private ReviewBoard reviewBoard;
 
     private LocalDateTime visitedAt;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ReviewBoardRecentVisit that = (ReviewBoardRecentVisit) o;
+        return Objects.equals(user, that.user) && Objects.equals(reviewBoard, that.reviewBoard);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(user, reviewBoard);
+    }
 }
