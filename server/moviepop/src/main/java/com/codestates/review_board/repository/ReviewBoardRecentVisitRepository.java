49267e6d0ffe6e9b9160ee9e3197e2a8b3dc830d
otaeho <dhxogh123123@naver.com>
2023-07-20 18:46:47 +0900

    fix: fix data type

--- server/moviepop/src/main/java/com/codestates/review_board/repository/ReviewBoardRecentVisitRepository.java
+++ server/moviepop/src/main/java/com/codestates/review_board/repository/ReviewBoardRecentVisitRepository.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 public interface ReviewBoardRecentVisitRepository extends JpaRepository<ReviewBoardRecentVisit, Long> {
     ReviewBoardRecentVisit findByUserAndReviewBoard(User user, ReviewBoard reviewBoard);
-    LocalDateTime findFirstByUserOrderByVisitedAtAsc(User user); // 필드 기반 첫번째 값 + Asc(오름차순 정렬)
+    ReviewBoardRecentVisit findFirstByUserOrderByVisitedAtAsc(User user); // 필드 기반 첫번째 값 + Asc(오름차순 정렬)
     ReviewBoardRecentVisit findByUserAndVisitedAt(User user, LocalDateTime visitedAt);
     List<ReviewBoardRecentVisit> findByUser(User user);
 }
