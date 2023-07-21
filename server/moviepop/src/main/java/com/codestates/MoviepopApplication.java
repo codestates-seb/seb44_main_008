9e683e4c8dca1b5d9c00d09941e40a9cc96bc7c2
otaeho <dhxogh123123@naver.com>
2023-07-21 13:22:29 +0900

    fix: change server time zone

--- server/moviepop/src/main/java/com/codestates/MoviepopApplication.java
+++ server/moviepop/src/main/java/com/codestates/MoviepopApplication.java
@@ -7,14 +7,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 
 @EnableJpaAuditing
 @EnableScheduling
 @SpringBootApplication
 public class MoviepopApplication {
+	@PostConstruct
+	public void setTimeZone(){
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MoviepopApplication.class, args);
 	}
-
 }
