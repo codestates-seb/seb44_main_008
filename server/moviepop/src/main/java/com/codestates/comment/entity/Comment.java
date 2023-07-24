297f682a1f69af615019521437ef0ccad312d14a
otaeho <dhxogh123123@naver.com>
2023-07-24 11:19:19 +0900

    feat: add mapping with CommentLike

--- server/moviepop/src/main/java/com/codestates/comment/entity/Comment.java
+++ server/moviepop/src/main/java/com/codestates/comment/entity/Comment.java
@@ -2,13 +2,16 @@ package com.codestates.comment.entity;
 
 import com.codestates.audit.Auditable;
 import com.codestates.review_board.entity.ReviewBoard;
+import com.codestates.user.entity.CommentLike;
 import com.codestates.user.entity.User;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @NoArgsConstructor
 @Getter
@@ -32,10 +35,17 @@ public class Comment extends Auditable {
     @JoinColumn(name = "USER_ID")
     private User user;
 
+    @OneToMany(mappedBy = "comment", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<CommentLike> commentLikes = new HashSet<>();
+
     public void setReviewBoard(ReviewBoard reviewBoard) {
         this.reviewBoard = reviewBoard;
         if(!reviewBoard.getComments().contains(this))
             reviewBoard.getComments().add(this);
+    }
+
+    public void addCommentLike(CommentLike commentLike) {
+        this.commentLikes.add(commentLike);
     }
 
     @Override
