d71d7cbfa1adfa471bb17f2214061b5e056a859c
otaeho <dhxogh123123@naver.com>
2023-10-19 15:52:42 +0900

    fix: change @Id import

--- server/moviepop/src/main/java/com/codestates/security/redis/certification/CertificationNumber.java
+++ server/moviepop/src/main/java/com/codestates/security/redis/certification/CertificationNumber.java
@@ -3,11 +3,9 @@ package com.codestates.security.redis.certification;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 import org.springframework.data.redis.core.TimeToLive;
-
-import javax.persistence.Id;
 
 @Getter
 @AllArgsConstructor
